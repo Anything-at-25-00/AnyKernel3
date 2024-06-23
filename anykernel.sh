686cc643c6647cfba25e2c9846c06d062b9e883d
dmpstr <aubrey.jmps@proton.me>
2024-06-23 13:41:52 +0800

    Update for A05
    Signed-off-by: dmpstr <aubrey.jmps@proton.me>

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,12 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Kernel at 25:00 - Galaxy A34 (mizuki)
-do.devicecheck=1
+kernel.string=Kernel at 25:00 - Galaxy A05 (k25/kanade)
+do.devicecheck=0
 do.modules=0
-do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=a34x
+device.name1=
 device.name2=
 device.name3=
 device.name4=
@@ -37,68 +36,35 @@ patch_vbmeta_flag=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-## end boot install
+ui_print "- Unpacking boot image";
 
 
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
+## AnyKernel install
+dump_boot;
 
-# init_boot shell variables
-#block=init_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+mount /system/
+mount /system_root/
 
-# reset for init_boot patching
-#reset_ak;
+# Change permissions
+chmod 755 /system/bin/busybox;
 
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
+# Deepsleep fix (@Chainfire)
+for i in `ls /sys/class/scsi_disk/`; do
+ cat /sys/class/scsi_disk/$i/write_protect 2>/dev/null | grep 1 >/dev/null
+ if [ $? -eq 0 ]; then
+  echo 'temporary none' > /sys/class/scsi_disk/$i/cache_type
+ fi
+done;
 
-#write_boot;
-## end init_boot install
+umount /system;
+umount /system_root;
 
+ui_print "- Installing new boot image";
 
-## vendor_kernel_boot shell variables
-#block=vendor_kernel_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+write_boot;
 
-# reset for vendor_kernel_boot patching
-#reset_ak;
+ui_print "- Done";
+ui_print " ";
 
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
+## end install
 
